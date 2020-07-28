62c55b7296d965af2813dae30673c93aa07a901e
carlos pizano <carlos.pizano@gmail.com>
2020-07-27 22:30:10 -0700

    first commit
    Using git unity plugin

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+//using System.Diagnostics;
+using System;
+using System.Reflection;
+
+public class Client : MonoBehaviour
+{
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+    public string ip = "127.0.0.1";
+    public int port = 26951;
+    public int myId = 0;
+    public TCP tcp;
+
+    public class TCP
+    {
+        public TcpClient socket;
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+
+        public void Connect()
+        {
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, null);
+        }
+
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+
+            if (!socket.Connected)
+            {
+                return;
+            }
+
+            stream = socket.GetStream();
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLenght = stream.EndRead(_result);
+                if (_byteLenght < 0)
+                {
+                    // TODO: disconnect.
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLenght];
+                Array.Copy(receiveBuffer, _data, _byteLenght);
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, new AsyncCallback(ReceiveCallback), null);
+
+            }
+            catch
+            {
+               
+                // TODO: disconnect client.
+            }
+        }
+    }
+ 
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Client Instance already exists! destroying");
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer()
+    {
+        tcp.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
