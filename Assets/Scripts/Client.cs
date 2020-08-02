8d2b97bf90013b6b58a92c78c0e5c44fd5147593
carlos pizano <carlos.pizano@gmail.com>
2020-08-01 17:18:19 -0700

    Second part
    TCP Hello

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -16,10 +16,39 @@ public class Client : MonoBehaviour
     public int myId = 0;
     public TCP tcp;
 
+    private delegate void PacketHandler(Packet _packet);
+    private static Dictionary<int, PacketHandler> packetHandlers;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Client Instance already exists! destroying");
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer()
+    {
+        InitializeClientData();
+        tcp.Connect();
+    }
+
     public class TCP
     {
         public TcpClient socket;
         private NetworkStream stream;
+        private Packet receivedData;
         private byte[] receiveBuffer;
 
         public void Connect()
@@ -44,7 +73,23 @@ public class Client : MonoBehaviour
             }
 
             stream = socket.GetStream();
+            receivedData = new Packet();
             stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        public void SendData(Packet _packet)
+        {
+            try
+            {
+                if (socket != null)
+                {
+                    stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
+                }
+            }
+            catch (Exception _ex)
+            {
+                Debug.Log($"Error sending data to server via TCP: {_ex}");
+            }
         }
 
         private void ReceiveCallback(IAsyncResult _result)
@@ -61,6 +106,7 @@ public class Client : MonoBehaviour
                 byte[] _data = new byte[_byteLenght];
                 Array.Copy(receiveBuffer, _data, _byteLenght);
 
+                receivedData.Reset(HandleData(_data));
                 stream.BeginRead(receiveBuffer, 0, dataBufferSize, new AsyncCallback(ReceiveCallback), null);
 
             }
@@ -70,35 +116,63 @@ public class Client : MonoBehaviour
                 // TODO: disconnect client.
             }
         }
+
+        private bool HandleData(byte[] _data)
+        {
+            int _packetLength = 0;
+            receivedData.SetBytes(_data);
+
+            if (receivedData.UnreadLength() >= 4)
+            {
+                _packetLength = receivedData.ReadInt();
+                if (_packetLength <= 0)
+                {
+                    return true;
+                }
+            }
+
+            while ((_packetLength > 0) && (_packetLength <= receivedData.UnreadLength()))
+            {
+                byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
+
+                Debug.Log("rvc data: HandleData2");
+
+                ThreadManager.ExecuteOnMainThread(() =>
+                {
+                    using (Packet _packet = new Packet(_packetBytes))
+                    {
+                        int _packetId = _packet.ReadInt();
+                        Debug.Log($"EOMT {_packetId}");
+                        packetHandlers[_packetId](_packet);
+                    }
+                });
+
+                _packetLength = 0;
+                if (receivedData.UnreadLength() >= 4)
+                {
+                    _packetLength = receivedData.ReadInt();
+                    if (_packetLength <= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (_packetLength <= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
  
-    private void Awake()
+    private void InitializeClientData()
     {
-        if (instance == null)
+        packetHandlers = new Dictionary<int, PacketHandler>()
         {
-            instance = this;
-        }
-        else if (instance != this)
-        {
-            Debug.Log("Client Instance already exists! destroying");
-            Destroy(this);
-        }
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        tcp = new TCP();
-    }
-
-    public void ConnectToServer()
-    {
-        tcp.Connect();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            { (int)ServerPackets.welcome, ClientHandle.Welcome }
+        };
+        Debug.Log("Initialized packets handlers.");
     }
 }
